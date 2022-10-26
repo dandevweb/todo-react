15ab1d9f9c98a16de0d9ff52ef833657a0282256
dandevweb <danilo.pacheco@soitic.com>
2022-10-26 08:04:54 -0300

    Recovering tasks from api

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { TaskEmpty } from './components/TaskEmpty';
 import { Counter } from './components/Counter';
 import { Tasks } from './components/Tasks';
 
+export const pathApi = 'http://localhost:8000/api/';
+export const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTY2Njc3ODgxNiwiZXhwIjoxNjY5MzcwODE2LCJuYmYiOjE2NjY3Nzg4MTYsImp0aSI6InBIaVNabUhFV2NRS0ZuR0UiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.V1KAGu7kbz1i1Ld6QzG4HcQmdA9nvLOyT84CztsP9EY';
+
 export function App() {
 
   return (
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from 'react';
+import { pathApi, token } from '../App';
 import styles from './Counter.module.css';
 
+interface Task {
+    id: number;
+    label: string;
+    is_complete: boolean;
+}
+
 export function Counter() {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const amount = tasks.length;
+
+    useEffect(() => {
+        fetch(pathApi + 'tasks', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token,
+            }
+        })
+            .then(response => response.json())
+            .then(data => setTasks(data.data))
+    }, []);
+
+    const completeTasks = tasks.filter(complete => complete.is_complete).length
+
     return (
         <header className={styles.headerCounter}>
             <div className={styles.info}>
                 <h3>Tarefas criadas</h3>
                 <div className={styles.counter}>
-                    <span>0</span>
+                    <span>{amount}</span>
                 </div>
             </div>
 
             <div className={styles.info}>
                 <h3>Concluídas</h3>
                 <div className={styles.counter}>
-                    <span>0</span>
+                    <span>{completeTasks}</span>
                 </div>
             </div>
         </header>
